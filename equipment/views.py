8e4573f6c0ee985347786309b6375a54b32d0bc2
Chris Reichert <chris.a.reichert@gmail.com>
2022-01-13 15:27:02 -0600

    first commit,plus made similar to previous project

---
+++ equipment/views.py
@@ -0,0 +1,11 @@
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
+from .models import Equip
+from .serializers import EquipSerializer
+
+class EquipList(ListCreateAPIView):
+    queryset = Equip.objects.all()
+    serializer_class = EquipSerializer
+
+class EquipDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Equip.objects.all()
+    serializer_class = EquipSerializer
